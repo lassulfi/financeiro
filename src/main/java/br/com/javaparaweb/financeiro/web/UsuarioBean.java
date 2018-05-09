efb5a02edb14f14e9f99ca139bd3788cb1a84388
Luis Daniel Assulfi <lassulfi@gmail.com>
2018-05-08 22:40:29 -0300

    Adição das páginas de cadastro e login de usuario

--- src/main/java/br/com/javaparaweb/financeiro/web/UsuarioBean.java
+++ src/main/java/br/com/javaparaweb/financeiro/web/UsuarioBean.java
@@ -1,9 +1,12 @@
 package br.com.javaparaweb.financeiro.web;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
 
 import br.com.javaparaweb.financeiro.usuario.Usuario;
+import br.com.javaparaweb.financeiro.usuario.UsuarioRN;
 
 @ManagedBean(name="usuarioBean")
 @RequestScoped
@@ -31,7 +34,26 @@ public class UsuarioBean {
 		this.confirmarSenha = confirmarSenha;
 	}
 	
-	//Getters & Setters
+	//Métodos
+	public String novo() {
+		this.usuario = new Usuario();
+		this.usuario.setAtivo(true);
+		return "/publico/usuario";
+	}
 	
-
+	public String salvar() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		
+		String senha = this.usuario.getSenha();
+		if (!senha.equals(this.confirmarSenha)) {
+			FacesMessage facesMessage = new FacesMessage("A senha não foi confirmada corretamente");
+			context.addMessage(null, facesMessage);
+			return null;
+		}
+		
+		UsuarioRN usuarioRN = new UsuarioRN();
+		usuarioRN.salvar(this.usuario);
+	
+		return "usuariosucesso";
+	}	
 }
