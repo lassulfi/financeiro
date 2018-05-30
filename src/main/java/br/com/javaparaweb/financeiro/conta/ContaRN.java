7f824c67aa285998d05be839de06c6eb1831058c
Luis Daniel Assulfi <lassulfi@gmail.com>
2018-05-29 22:26:27 -0300

    Atualização das classes de conta, nas camadas DAO e RN e interface web

---
+++ src/main/java/br/com/javaparaweb/financeiro/conta/ContaRN.java
@@ -0,0 +1,58 @@
+package br.com.javaparaweb.financeiro.conta;
+
+import java.util.Date;
+import java.util.List;
+
+import br.com.javaparaweb.financeiro.usuario.Usuario;
+import br.com.javaparaweb.financeiro.util.DAOFactory;
+
+/**
+ * Classe com a Regra de Negocio da classe conta.
+ * @author lassulfi
+ *
+ */
+public class ContaRN {
+	
+	//Atributos
+	private ContaDAO contaDAO;
+	
+	//Construtor
+	public ContaRN() {
+		this.contaDAO = DAOFactory.criarContaDAO();
+	}
+	
+	//Métodos
+	
+	public List<Conta> listar(Usuario usuario){
+		return this.contaDAO.listar(usuario);
+	}
+	
+	public Conta carregar(Integer conta) {
+		return this.contaDAO.carregar(conta);
+	}
+	
+	public void salvar(Conta conta) {
+		conta.setDataCadastro(new Date());
+		this.contaDAO.salvar(conta);
+	}
+	
+	public void excluir(Conta conta) {
+		this.contaDAO.excluir(conta);
+	}
+	
+	public void tornarFavorita(Conta contaFavorita) {
+		Conta conta = this.buscarFavorita(contaFavorita.getUsuario());
+		if (conta != null) {
+			conta.setFavorita(false);
+			this.contaDAO.salvar(conta);
+		}
+		
+		contaFavorita.setFavorita(true);
+		this.contaDAO.salvar(contaFavorita);
+	}
+
+	public Conta buscarFavorita(Usuario usuario) {
+		return this.contaDAO.buscarFavorita(usuario);
+	}
+
+}
